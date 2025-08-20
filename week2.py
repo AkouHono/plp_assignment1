e2bbfbdb5a4e9f4c8b49a60058bf76b13eac6487
AkouHono <akouvigbadabizo@gmail.com>
2025-08-20 11:39:35 +0530

    Add Week2 Assignment solution

---
+++ week2.py
@@ -0,0 +1,29 @@
+#number1
+my_list = []
+# number2 append of the following element to the list my_list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# number3 insertion of 15 at the second position of the  list 
+
+my_list.insert(1, 15)
+
+# number4 extending of my_list with other list
+my_list.extend([50, 60, 70])
+
+#number5 remove the last element from my_list
+my_list.pop()
+
+#number6 Sort my_list in ascending order
+my_list.sort()
+
+#number7 the value 30 in my_list
+
+index_of_30 = my_list.index(30)
+
+#print of the index of 30
+
+print("Index of 30 :", index_of_30)
